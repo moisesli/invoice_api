c545e4f4ed39e354e1b24a8df97c08046e9e3af5
moises <moises@iMac-de-moises.local>
2020-03-12 18:07:56 -0500

    invoice

--- test/loginTest.php
+++ test/loginTest.php
@@ -7,11 +7,23 @@ include "../vendor/autoload.php";
 
 class LoginTest extends TestCase
 {
+    protected $client;
+
     public function testLogin()
     {
-        $client = new GuzzleHttp\Client();
-        $response = $client->get('http://invoice_api.localhost/src/login.php?f=login');
-        var_dump($response->getBody()->getContents());
+        $this->client = new GuzzleHttp\Client([
+            'base_uri' => 'http://invoice_api.localhost/src/'
+        ]);
+
+        $response = $this->client->get('/login.php', [
+            'query' => [
+                'f' => 'login'
+            ]
+        ]);
+
+//        $client = new GuzzleHttp\Client();
+//        $response = $client->get('http://invoice_api.localhost/src/login.php?f=login');
+//        var_dump($response->getBody()->getContents());
         $this->assertEquals(true,true);
     }
 }
